311f444d74d8b6d29139854d0569a980e63d5db9
Andreas Haerter <development@andreas-haerter.com>
2011-11-24 03:18:07 +0100

    added Spanish language files

---
+++ lang/es/lang.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Spanish language for the "mnml-blog" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/lang.php"
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Adrián Rodríguez Vargas <info@adrianrodriguez.es>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-mnml-blog
+ * @link http://www.dokuwiki.org/template:mnml-blog
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//links (not used by default but useful for /user/boxes.php)
+$lang["mnmlblog_lnk_print"] = "Versión para imprimir";
+$lang["mnmlblog_lnk_permrev"] = "Categoría Padre";
+$lang["mnmlblog_lnk_whatlinkshere"] = "Qué enlace es este";
+$lang["mnmlblog_lnk_upload"] = "Subir fichero";
+$lang["mnmlblog_lnk_siteindex"] = "Página de inicio";
+
+//other
+$lang["mnmlblog_search"] = "Búsqueda";
+$lang["mnmlblog_accessdenied"] = "Aceso restringido";
+$lang["mnmlblog_fillplaceholder"] = "Por favor, rellena este campo";
+$lang["mnmlblog_donate"] = "Donar";
+$lang["mnmlblog_mdtemplatefordw"] = "Plantilla mnml-blog para DokuWiki";
+$lang["mnmlblog_recentchanges"] = "Cambios recientes";
+
---
+++ lang/es/settings.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Spanish language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/lang.php"
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Adrián Rodríguez Vargas <info@adrianrodriguez.es>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-mnml-blog
+ * @link http://www.dokuwiki.org/template:mnml-blog
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//header navigation
+$lang["mnmlblog_headernav"]          = "¿Mostrar categorías de navegación?";
+$lang["mnmlblog_headernav_location"] = "En caso afirmativo, usar la siguiente wiki como principal:";
+
+//sidebar navigation
+$lang["mnmlblog_sidebarnav"]          = "¿Mostrar barra de navegación?";
+$lang["mnmlblog_sidebarnav_location"] = "En caso afirmativo, usar la siguiente página como barra de navegación:";
+
+//custom copyright notice
+$lang["mnmlblog_copyright"]          = "¿Mostrar copyright?";
+$lang["mnmlblog_copyright_default"]  = "En caso afirmativo, ¿usar el copyright por defecto?";
+$lang["mnmlblog_copyright_location"] = "Si no hay por defecto, usar la siguiente página como licencia de copyright:";
+
+//search form
+$lang["mnmlblog_search"]    = "¿Mostrar formulario de búsqueda?";
+$lang["mnmlblog_search_pos"] = "En caso afirmativo, ¿En que posición quieres mostrarl?";
+
+//other stuff
+$lang["mnmlblog_newpostform_location"] = "Usar la siguiente página para el formulario '&lt;blog newform&gt;'";
+$lang["mnmlblog_loaduserjs"]           = "¿Cargar 'mnml-blog/user/user.js'?";
+
