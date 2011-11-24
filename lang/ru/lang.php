d77b91a0592d0091f1fe5e823073e59304799534
Andreas Haerter <development@andreas-haerter.com>
2011-11-24 02:34:39 +0100

    added Russian language files

---
+++ lang/ru/lang.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * English language for the "mnml-blog" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/lang.php"
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Дмитрий Ефимов (Dmitriy Efimov)
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-mnml-blog
+ * @link http://www.dokuwiki.org/template:mnml-blog
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//links (not used by default but useful for /user/boxes.php)
+$lang["mnmlblog_lnk_print"] = "Версия для печати";
+$lang["mnmlblog_lnk_permrev"] = "Постоянная ссылка";
+$lang["mnmlblog_lnk_whatlinkshere"] = "Ссылки сюда";
+$lang["mnmlblog_lnk_upload"] = "Загрузить файд";
+$lang["mnmlblog_lnk_siteindex"] = "Индекс сайта";
+
+//other
+$lang["mnmlblog_search"] = "Поиск";
+$lang["mnmlblog_accessdenied"] = "Доступ запрещён";
+$lang["mnmlblog_fillplaceholder"] = "Заполните этот прототип";
+$lang["mnmlblog_donate"] = "Пожертвование";
+$lang["mnmlblog_mdtemplatefordw"] = "Шаблон mnml-blog для DokuWiki";
+$lang["mnmlblog_recentchanges"] = "Последние изменения";
+
---
+++ lang/ru/settings.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * English language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/lang.php"
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Дмитрий Ефимов (Dmitriy Efimov)
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-mnml-blog
+ * @link http://www.dokuwiki.org/template:mnml-blog
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//header navigation
+$lang["mnmlblog_headernav"]          = "Показывать навигацию в заголовке?";
+$lang["mnmlblog_headernav_location"] = "Если да, использовать эту wiki страницу как навигацию в заголовке:";
+
+//sidebar navigation
+$lang["mnmlblog_sidebarnav"]          = "Показывать боковую навигацию?";
+$lang["mnmlblog_sidebarnav_location"] = "Если да, использовать эту wiki страницу как боковую навигацию:";
+
+//custom copyright notice
+$lang["mnmlblog_copyright"]          = "Показывать уведомление об авторских правах?";
+$lang["mnmlblog_copyright_default"]  = "Если да, использовать уведомление по умолчанию для авторских прав?";
+$lang["mnmlblog_copyright_location"] = "Если не по умолчанию, использовать эту wiki страницу как уведомление об авторских правах:";
+
+//search form
+$lang["mnmlblog_search"]    = "Показывать форму поиска?";
+$lang["mnmlblog_search_pos"] = "Если да, позиция формы поиска";
+
+//other stuff
+$lang["mnmlblog_newpostform_location"] = "Использовать эту wiki страницу для формы '&lt;blog newform&gt;':";
+$lang["mnmlblog_loaduserjs"]           = "Загрузить 'mnml-blog/user/user.js'?";
+
