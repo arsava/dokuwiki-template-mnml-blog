78b9590cdd92b9c0fade226986fc88a9adcdbdc5
Andreas Haerter <development@andreas-haerter.com>
2011-12-03 01:45:12 +0100

    remove 'last modified on' from RSS feed template

--- blogtng-tpl/feed.php
+++ blogtng-tpl/feed.php
@@ -25,14 +25,10 @@
 
 <p>
     <small>
-        This blog post was created <?php $entry->tpl_created("on %Y-%m-%d at %H:%M")?>
-        <?php if ($entry->entry['created'] != $entry->entry['lastmod']) {?>
-            and last modified <?php $entry->tpl_lastmodified("on %Y-%m-%d at %H:%M")?>
-        <?php }?>
-        by
+        This blog post was created <?php $entry->tpl_created("on %Y-%m-%d at %H:%M")?> by
         <?php $entry->tpl_author()?>.
         <?php if ($entry->has_tags()):?>
-            It is tagged with <?php $entry->tpl_tagstring("")?>.
+        It is tagged with <?php $entry->tpl_tagstring("")?>.
         <?php endif ?>
     </small>
 </p>
