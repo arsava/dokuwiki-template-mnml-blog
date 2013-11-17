b27d8d2d529e02291eccfaf77d6d6d972d6c80db
Andreas Haerter <ah@foundata.net>
2013-11-17 14:03:14 +0100

    Add Korean language files

---
+++ lang/ko/lang.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Korean language for the "mnml-blog" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/lang.php"
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones.
+ *
+ * Don't forget to mail your translation to
+ *   Andreas Haerter <development@andreas-haerter.com>
+ * Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Myeongjin <aranet100@gmail.com>
+ * @link http://www.dokuwiki.org/template:mnml-blog
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//links (not all are used by default but may be useful for /user/boxes.php)
+$lang["mnmlblog_lnk_print"] = "인쇄용 문서";
+$lang["mnmlblog_lnk_permrev"] = "고유 링크";
+$lang["mnmlblog_lnk_whatlinkshere"] = "여기를 가리키는 문서";
+$lang["mnmlblog_lnk_siteindex"] = "사이트맵";
+$lang["mnmlblog_lnk_upload"] = "파일 올리기";
+$lang["mnmlblog_lnk_newposting"] = "새 포스트";
+
+//qr code box
+$lang["mnmlblog_qrcodebox_qrcode"] = "QR 코드: 현재 문서의 URL";
+$lang["mnmlblog_qrcodebox_genforcurrentpage"] = "현재 문서의 생성된 QR 코드";
+$lang["mnmlblog_qrcodebox_urlofcurrentpage"] = "현재 문서의 QR 코드 (쉽게 모바일 접근을 하려면 스캔)";
+
+//other
+$lang["mnmlblog_search"] = "찾기";
+$lang["mnmlblog_accessdenied"] = "접근 거부됨";
+$lang["mnmlblog_fillplaceholder"] = "이 자리를 채우거나 비활성화하세요";
+$lang["mnmlblog_donate"] = "기부";
+$lang["mnmlblog_mdtemplatefordw"] = "도쿠위키를 위한 mnml-blog 템플릿";
+$lang["mnmlblog_recentchanges"] = "최근 바뀜";
+
---
+++ lang/ko/settings.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Korean language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/lang.php"
+ * - "/lib/tpl/mnml-blog/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones.
+ *
+ * Don't forget to mail your translation to
+ *   Andreas Haerter <development@andreas-haerter.com>
+ * Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Myeongjin <aranet100@gmail.com>
+ * @link http://www.dokuwiki.org/template:mnml-blog
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//header navigation
+$lang["mnmlblog_headernav"]          = "머리글 둘러보기를 보여줄까요?";
+$lang["mnmlblog_headernav_location"] = "만약 보여준다면 머리글 둘러보기로 다음 위키 문서 사용:";
+
+//sidebar navigation
+$lang["mnmlblog_sidebarnav"]          = "사이드바 둘러보기를 보여줄까요?";
+$lang["mnmlblog_sidebarnav_location"] = "만약 보여준다면 사이드바 둘러보기로 다음 위키 문서 사용:";
+
+//custom copyright notice
+$lang["mnmlblog_copyright"]          = "저작권 알림을 보여줄까요?";
+$lang["mnmlblog_copyright_default"]  = "만약 보여준다면 기본 저작권 알림을 사용하겠습니까?";
+$lang["mnmlblog_copyright_location"] = "기본 알림을 사용하지 않는다면 저작권 알림으로 다음 위키 문서 사용:";
+
+//search form
+$lang["mnmlblog_search"]    = "검색 양식을 보여줄까요?";
+$lang["mnmlblog_search_pos"] = "만약 보여준다면 검색 양식 위치";
+
+//qr code box
+$lang["mnmlblog_qrcodebox"] = "(모바일 브라우저에 쉽게 URL 전송을 위해) 현재 위키 문서 URL의 QR 코드로 된 상자를 보여줄까요?";
+
+//other stuff
+$lang["mnmlblog_newpostform_location"]   = "'&lt;blog newform&gt;' 양식에 대해 다음 위키 문서 사용:";
+$lang["mnmlblog_hideadminlinksfromanon"] = "클라이언트가 인증된 사용자가 아니면 모든 관리자와 사용자 기능과 관련된 링크를 숨길까요? 이 기능이 활성이면 수동으로 로그인 양식을 호출해야 함에 주의하세요. (힌트: '".DOKU_URL.DOKU_SCRIPT."?do=login')";
+$lang["mnmlblog_loaduserjs"]             = "'mnml-blog/user/user.js'를 불러올까요?";
+
